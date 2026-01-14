b334e2eadf6e66f05616baa9bca4608ff18dba55
Cyborus <cyborus@disroot.org>
2026-01-14 11:50:46 -0500

    feat(login): `v14.next.forgejo.org`

--- src/auth.rs
+++ src/auth.rs
@@ -143,6 +143,7 @@ pub async fn get_client_info_for(url: &url::Url) -> eyre::Result<Option<String>>
         "v11.next.forgejo.org" => "0df6d672-fe05-4c9a-a5a9-e111e4905e14",
         "v12.next.forgejo.org" => "df333c23-09a7-41ee-ad52-de673166dbb8",
         "v13.next.forgejo.org" => "ef27a227-65f4-4bcb-be56-f8c9b44457b0",
+        "v14.next.forgejo.org" => "2dc5d6d7-01b0-47b4-814e-b4b60aea2376",
         "git.disroot.org" => "c6051ae0-6d21-4c17-92e6-41b957376d09",
         "git.pub.solar" => "6c7fad2f-41c4-4c2d-90b2-5f7fd19c9be2",
         "git.kaki87.net" => "951299e6-cf99-4a9e-8aaf-4b4b4ac36f04",
