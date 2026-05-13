5640d3dbaa8c49693aa580c8283802932c7d7b45
Cyborus <cyborus@disroot.org>
2026-05-13 12:10:04 -0400

    build: display ftl errors as errors instead of warnings

--- build.rs
+++ build.rs
@@ -19,14 +19,14 @@ fn check_fluent_file(path: &std::path::Path) {
         }
     };
     if let Err((_, errs)) = fluent_syntax::parser::parse(file.as_str()) {
-        let pre = "cargo::warning=";
-        let yellow = "\x1b[93m";
+        let pre = "cargo::error=";
+        let red = "\x1b[91m";
         let blue = "\x1b[94m";
         let bold = "\x1b[1m";
         let reset_fg = "\x1b[39m";
         let reset = "\x1b[0m";
         for err in errs {
-            println!("{pre}{yellow}{bold}warning{reset_fg}: {}{reset}", err.kind);
+            println!("{pre}{red}{bold}error{reset_fg}: {}{reset}", err.kind);
             let line_number = &file[..err.pos.start].lines().count() + 1;
             println!("{pre} {blue}-->{reset} {}:{line_number}", path.display());
             if let Some(slice) = err.slice {
@@ -39,7 +39,7 @@ fn check_fluent_file(path: &std::path::Path) {
                     if err.pos.start < current_offset + line.len() && err.pos.end > current_offset {
                         let spaces = err.pos.start.saturating_sub(current_offset);
                         let arrows = (err.pos.end - current_offset).min(line.len()) - spaces;
-                        println!("{pre}  {bold}{blue}|{reset} {:spaces$}{bold}{yellow}{:^<arrows$}{reset}", "", "");
+                        println!("{pre}  {bold}{blue}|{reset} {:spaces$}{bold}{red}{:^<arrows$}{reset}", "", "");
                         has_last_line = true;
                     }
                     current_offset += line.len() + 1;
