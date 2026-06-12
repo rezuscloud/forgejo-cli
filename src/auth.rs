4a5e2f2f11e7ffe45c87c9d3c345007891a4b481
Cyborus <cyborus@disroot.org>
2026-06-12 15:03:29 -0400

    chore(login): remove id for v7.next.forgejo.org

--- src/auth.rs
+++ src/auth.rs
@@ -155,7 +155,6 @@ pub async fn get_client_info_for(url: &url::Url) -> eyre::Result<Option<String>>
     let builtin = match host {
         "codeberg.org" => "19ac3dd0-e101-445d-aa60-d8ea3876bc5d",
         "code.forgejo.org" => "ab67d8a2-72bd-42e8-ae05-937eaba31e24",
-        "v7.next.forgejo.org" => "adf79db0-0e6c-41d8-93a9-3c13e797e880",
         "v11.next.forgejo.org" => "0df6d672-fe05-4c9a-a5a9-e111e4905e14",
         "v12.next.forgejo.org" => "df333c23-09a7-41ee-ad52-de673166dbb8",
         "v13.next.forgejo.org" => "ef27a227-65f4-4bcb-be56-f8c9b44457b0",
